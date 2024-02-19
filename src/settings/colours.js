210307ec29534eafe6b436a258a129d9d4fdd688
CamHowling <>
2024-02-19 17:31:43 +1100

    update colours

--- src/settings/colours.js
+++ src/settings/colours.js
@@ -1,30 +1,28 @@
 //Faction Colours
-export const colourFremen = '#CA842A';
-export const colourAtreides = '#49642B';
-export const colourHarkonen = '#222326';
-export const colourBeneGesserit = '#1B3366';
-export const colourSpacingGuild = '#C3491E';
-export const colourEmperor = '#8A1D16';
-export const colourTleilaxu = '#2C1A48'; 
-export const colourIxian = '#7C7E82';
-export const colourChoam = '#CD2515';
-export const colourRicheseGrey = '#CACCBF'; 
-export const colourRicheseBlue = '#292A30';
+export const fremen = '#CA842A';
+export const atreides = '#49642B';
+export const harkonen = '#222326';
+export const beneGesserit = '#1B3366';
+export const spacingGuild = '#C3491E';
+export const emperor = '#8A1D16';
+export const tleilaxu = '#2C1A48'; 
+export const ixian = '#7C7E82';
+export const choam = '#CD2515';
+export const richeseGrey = '#CACCBF'; 
+export const richeseBlue = '#292A30';
+export const ecaz = '#93348E';
+export const moritani = '#3F95B4';
 
 //App Background, Headings, and Text
-export const colourHeadingMaj = '#171D29';
-export const colourHeadingMin = '#20334D';
-export const colourMainBG = '#FFFCF0';
-export const colourFooterTrans1 = '#E8D293';
-export const colourFooterTrans2 = '#B6A66C';
-export const colourBGText = '#3F1F19';
+export const majorHeading = '#171D29';
+export const minorHeading = '#20334D';
+export const mainBackground = '#FFFCF0';
+export const footerTransitionTop = '#E8D293';
+export const footerTransitionMiddle = '#B6A66C';
+export const footerTransitionBottom = '#3F1F19';
 
 //Card Title Backgrounds
-export const colourDarkRed = '#833031';
-export const colourLightRed = '#A43831';
-export const colourDarkPurple = '#2E2A41';
-export const colourLightPurple = '#3D3C5b';
-export const colourDarkTan = '#7E663A';
-export const colourLightTan = '#AF986C';
-export const colourDarkBrown = '#413429';
-export const colourLightBrown = '#73654A';
+export const treacheryRed = '#833031';
+export const treacheryBlue = '#2E2A41';
+export const treacheryTan = '#7E663A';
+export const treacheryGreen = '#2d350f';
